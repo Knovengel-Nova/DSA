7c7c0ddd28a951a188d7c06778d85a2e42c34243
Knovengel-Nova <knovengel@gmail.com>
2025-07-21 22:01:38 +0530

    Problem 2 of easy Problems solved (Trial 1)

--- src/dsa/array/easy/Problem2.java
+++ src/dsa/array/easy/Problem2.java
@@ -15,6 +15,23 @@ package dsa.array.easy;
  */
 public class Problem2 {
     public static void main(String[] args){
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 3, 2, 4, 5};
+        System.out.println("Result : "+isSorted(arr2));
+    }
+    
+    public static boolean isSorted(int[] arr){
         
+        for(int i=1, j=0; i<arr.length; i++, j++){
+            System.out.println("i: "+i+"("+arr[i]+") j: "+j+"("+arr[j]+").\t is("+arr[j]+">"+arr[i]+")? : "+(arr[j]>arr[i]));
+            if(arr[j]> arr[i]){
+                return false;
+            }
+        }
+        return true;
     }
 }
+/*
+    This is the best solution.
+    Time Complexity : O(n).
+*/
