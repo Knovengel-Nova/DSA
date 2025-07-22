b072030f9cb9e05f5b21e4addc0670e97d735485
Knovengel-Nova <knovengel@gmail.com>
2025-07-22 19:54:35 +0530

    Problem 1 of medium Problems solved (Trial 1)

--- src/dsa/array/medium/Problem1.java
+++ src/dsa/array/medium/Problem1.java
@@ -14,6 +14,36 @@ package dsa.array.medium;
  */
 public class Problem1 {
     public static void main(String[] args){
+        int[] arr = {2, 7, 11, 15};
+        int target = 26;
+        int[] res = getSolution(arr, target);
+        System.out.println("Target "+target+((res == null)?(" was not in the arr."):(" found at the location ("+res[0]+", "+res[1]+").")));
+    }
+    
+    public static int[] getSolution(int[] arr, int target){
+        int[] op = {0, 0};
         
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] + arr[j] == target){
+                    op[0] = i;
+                    op[1] = j;
+                    return op;
+                }
+            }
+        }
+        
+        return null;
     }
 }
+
+/*
+    This is the best solution is you only use arrays.
+    Time complexity : O(n^2).
+
+    but we can sort the array first useing merge sort and then use two pointer approach.
+    Time complexity : O(nlog(n)).
+
+    else two sum problem is best solved using hash map/hash table.
+    Time complexity : O(n).
+*/
